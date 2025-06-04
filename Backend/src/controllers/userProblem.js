2ee831abe67bb37c5784440e159bce4453350a62
Nimesh <pandeynimesh778@gmail.com>
2025-06-04 23:41:01 +0530

    creating problem routes and controllers

--- Backend/src/controllers/userProblem.js
+++ Backend/src/controllers/userProblem.js
@@ -1,46 +1,61 @@
-const {getLanguageById,submitBatch}=require("../utils/problemUtility")
+const { getLanguageById, submitBatch, submitToken } = require("../utils/problemUtility");
+const Problem=require("../models/problem");
+const createProblem=async (req,res)=>{
+
+    const {
+        title,description,difficulty,tags,visibleTestCases, hiddenTestCases,startCode,
+    referenceSolution,problemCreator
+    }=req.body;
+
+    try{
+
+        for(const {language,completeCode} of referenceSolution){
+
+        const languageId=getLanguageById(language)
+
+        const submissions = visibleTestCases.map((testcase)=>({
+            source_code:completeCode,
+            language_id:languageId,
+            stdin:testcase.input,
+            expected_output:testcase.output
+        }));
+      
+
+        const submitResult=await submitBatch(submissions);
 
 
-const createProblem=async(req,res)=>{
 
-const {title,description,difficulty,tags,visibleTestCases,hiddenTestCases,
-    startCode,referenceSolution,probleCreator}=req.body;
-
-try{
-
-    for(const {language,completeCode} of referenceSolution){
-
-//format of judge zero
-//source code
-//language id
-const languageId=getLanguageById(language);
-// stdin
-//expectedOutput
-const submissions=visibleTestCases.map((data)=>({
-
-    source_code:data.completeCode,
-    language_id: data.languageId,
-    stdin: input,
-    expected_output:output
+        const resultToken = submitResult.map((value)=>value.token);
+        const testResult = await submitToken(resultToken);
+        for(const test of testResult){
+            if(test.status_id!=3){
+               return res.status(400).send("Error in your test result");
+            }     
+        }
 
 
-}));
+        }
 
 
-const submitResult = submitBatch(submissions);
+         const userProblem = await Problem.create({
+            ...req.body,
+            problemCreator:req.result._id
+         });
 
-}
+
+    res.status(201).send("Problem Saved Successfully");
 
     }
 
-   
-   
-    catch(err){
+    catch(error){
+        res.status(400).send("Error")
+    }
+
+
 
 
 }
 
-}
 
 
-
+module.exports={createProblem}
--- Backend/src/utils/problemUtility.js
+++ Backend/src/utils/problemUtility.js
@@ -1,29 +1,24 @@
 
- const axios=require('axios');
+const axios = require('axios');
 
+const getLanguageById = (lang)=>{
 
-
-const getLanguageById=(lang)=>{
-
-const language= {
-    "c++": 54,
-    "java":  62  ,
-    "javaScript": 63 
-}
-return language(lang.toLowerCase());
-
+    const language={
+"c++":54,
+"java":62,
+"javaScript":63
+    }
+return language[lang.toLowerCase()];
 
 }
 
+const submitBatch = async (submissions)=>{
 
-const submitBatch=  async (submissions)=>{
-
-
-const options = {
+    const options = {
   method: 'POST',
   url: 'https://judge0-ce.p.rapidapi.com/submissions/batch',
   params: {
-    base64_encoded: 'flase'
+    base64_encoded: 'false'
   },
   headers: {
     'x-rapidapi-key': 'f7a4bcd4bdmsh06bf7791aef0572p1c11b2jsn1c91e92de6de',
@@ -32,21 +27,63 @@ const options = {
   },
   data: {
     submissions
-    
   }
 };
 
 async function fetchData() {
 	try {
 		const response = await axios.request(options);
-		return res.data
+		return response.data;
 	} catch (error) {
 		console.error(error);
 	}
 }
 
-return await fetchData();
-
+   return await fetchData();
 
 }
-module.exports={getLanguageById,submitBatch};
+
+
+const waiting = (timer) => new Promise(resolve => setTimeout(resolve, timer));
+
+
+const submitToken = async (resultToken)=>{
+
+const options = {
+  method: 'GET',
+  url: 'https://judge0-ce.p.rapidapi.com/submissions/batch',
+  params: {
+    tokens: resultToken.join(","),
+    base64_encoded: 'false',
+    fields: '*'
+  },
+  headers: {
+    'x-rapidapi-key': 'f7a4bcd4bdmsh06bf7791aef0572p1c11b2jsn1c91e92de6de',
+    'x-rapidapi-host': 'judge0-ce.p.rapidapi.com'
+  }
+};
+
+async function fetchData() {
+	try {
+		const response = await axios.request(options);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+while(true){
+
+    const result = await fetchData();
+    const testResult = result.submissions.every((r)=>r.status_id>2);
+    if(testResult)
+    return result.submissions;
+
+    await waiting(1000);
+
+}
+    
+}
+
+
+module.exports = {getLanguageById,submitBatch,submitToken}
